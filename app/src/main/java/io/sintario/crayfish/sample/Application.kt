8a593438a09dfe5cc9e10ea2cafcdcd0aec96764
Chronum Sintario <chronum.sintario@gmail.com>
2018-02-28 10:16:11 +0900

    Initilalize Crayfish in sample code.

--- app/src/main/java/io/sintario/crayfish/sample/Application.kt
+++ app/src/main/java/io/sintario/crayfish/sample/Application.kt
@@ -6,5 +6,6 @@ import android.app.Application as AndroidApplication
 class Application: AndroidApplication() {
     override fun onCreate() {
         super.onCreate()
+        Crayfish.initialize(this)
     }
 }
