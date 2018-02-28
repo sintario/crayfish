dd3e36b8b4824a4f1ea1fc65e8ad43dbda19d272
Chronum Sintario <chronum.sintario@gmail.com>
2018-02-28 10:14:38 +0900

    initial commit.

---
+++ app/src/main/java/io/sintario/crayfish/sample/Application.kt
@@ -0,0 +1,10 @@
+package io.sintario.crayfish.sample
+
+import io.sintario.crayfish.Crayfish
+import android.app.Application as AndroidApplication
+
+class Application: AndroidApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
