9f8e24bbe2d69ed969e3e47f142741f99b7f1fe5
Chronum Sintario <chronum.sintario@gmail.com>
2018-02-28 10:57:21 +0900

    fix Case definition.
     - properties should be `var`
     - needs default constructor without arguments.

--- library/src/main/java/io/sintario/crayfish/model/Case.kt
+++ library/src/main/java/io/sintario/crayfish/model/Case.kt
@@ -3,8 +3,12 @@ package io.sintario.crayfish.model
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
-class Case(
-        @PrimaryKey val name: String,
-        val variant: Int,
-        val metadata: String? = null
-) : RealmObject()
+open class Case(
+        @PrimaryKey var name: String = "",
+        var variant: Int = DEFAULT_VARIANT,
+        var metadata: String? = null
+) : RealmObject() {
+    companion object {
+        const val DEFAULT_VARIANT = -1
+    }
+}
